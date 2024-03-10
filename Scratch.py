90dfbdc7da32e5d7cba14cd6dbab470c8f2eeb38
Andrew Marin <drew_marin@msn.com>
2024-03-09 19:40:12 -0600

    Updates for 3/9/24

--- Scratch.py
+++ Scratch.py
@@ -1,3 +1,7 @@
+from pwn import *
+import requests
+import sys
+
 def funtion3(s):
    print("\t{}".format(s))
 
@@ -6,20 +10,15 @@ funtion3("parameter")
 add_4 = lambda x : x + 4
 print(add_4(5))
 
-import sys
-
 print(sys.version)
 print(sys.executable)
 print(sys.platform)
 
-
-import requests
 x = requests.get('http://httpbin.org/get')
 print(x.content)
-
-from pwn import *
 
 print(cyclic(50))
 print(cyclic_find("laaa"))
 
-print(shellcraft.sh)
+print(p32(0x13371337))
+print(hex(u32(p32(0x13371337))))
